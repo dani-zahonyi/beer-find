186a170ecf302fa63b3e5d64216045dec5aa31d9
Záhonyi Dani <daniel.zahonyi@abrisconsult.com>
2020-03-14 13:40:55 +0100

    add starter loginpage

---
+++ src/scenes/login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./loginPage.css";
+import { LoginForm } from ".";
+
+export default () => {
+    return (
+        <div className="loginPage">
+            <LoginForm />
+        </div>
+    );
+};
---
+++ src/scenes/login/index.js
@@ -0,0 +1,5 @@
+import Login from "./Login";
+
+import LoginForm from "./components/LoginForm";
+export { LoginForm };
+export default Login;
