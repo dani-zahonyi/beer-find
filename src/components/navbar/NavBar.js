dac5669b9c8a0792d318dd3bac426dd55d9faa46
Záhonyi Dani <daniel.zahonyi@abrisconsult.com>
2020-03-14 13:38:03 +0100

    add navbar

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navBar.css";
+import { Logo } from ".";
+export default props => {
+    return (
+        <nav>
+            <Logo />
+        </nav>
+    );
+};
---
+++ src/components/navbar/index.js
@@ -0,0 +1,6 @@
+import NavBar from "./NavBar";
+import Logo from "./components/Logo";
+
+export { Logo };
+
+export default NavBar;
