f73c606e6cf39024ef5043699be1ee6d5bd231c9
Záhonyi Dani <daniel.zahonyi@abrisconsult.com>
2020-03-14 22:53:33 +0100

    show navbar in 2 row if screen is less than 640px

--- src/components/navbar/components/UserData.js
+++ src/components/navbar/components/UserData.js
@@ -1,15 +1,13 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../../context/AppContext";
-import colors from "../../../constants/colors";
 
 const UserData = () => {
     const { username, logout } = useContext(AppContext);
     return (
-        <>
-            {" "}
+        <div>
             <span className="welcomeText">Hi, {username}!</span>
             <button onClick={logout}>Logout</button>
-        </>
+        </div>
     );
 };
 
